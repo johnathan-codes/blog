384ea0da019e462b38e3ef11b718d7f3763b38a0
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-10-17 21:34:31 +0200

    adding disquss

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import styles from '../../styles/Home.module.scss'
 
 const { BLOG_URL, CONTENT_API_KEY } = process.env
@@ -37,20 +38,43 @@ type Post = {
 
 const Post: React.FC<{ post: Post }> = (props) => {
 	const { post } = props
+	const [enableLoadComments, setEnableLoadComments] = useState(true)
 	const router = useRouter()
 
 	if (router.isFallback) {
 		return <h1>Loading...</h1>
 	}
 
+	const loadComments = () => {
+		setEnableLoadComments(false)
+		;(window as any).disqus_config = function () {
+			this.page.url = window.location.href
+			this.page.identifier = post.slug
+		}
+
+		const disqus = document.createElement('script')
+		disqus.src = 'https://blog-next-ghost.disqus.com/embed.js'
+		disqus.setAttribute('data-timestamp', Date.now().toString())
+
+		document.body.appendChild(disqus)
+	}
+
 	return (
 		<div className={styles.container}>
-			<Link href="/">
-				{/* <a>Back</a> */}
-				Back
-			</Link>
+			<p className={styles.goback}>
+				<Link href="/">
+					{/* <a>Back</a> */}
+					Back
+				</Link>
+			</p>
 			<h1>{post.title}</h1>
 			<div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+			{enableLoadComments && (
+				<button className={styles.goback} onClick={loadComments}>
+					Load Comments
+				</button>
+			)}
+			<div id="disqus_thread"></div>
 		</div>
 	)
 }
