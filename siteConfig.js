8329fbbf08e5f981b6fa6f0550e8617de9deff43
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-10-18 21:57:20 +0200

    gatsby - styxlab

---
+++ siteConfig.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // Do not include a trailing slash!
+    siteUrl: `https://johnathan.codes`,
+
+    // Enable infinite scroll (default: true)
+    infiniteScroll: true,
+
+    // Initial number fetched, scrolling lazy loads posts one by one
+    // If infinite scroll is disabled: maximum number of post shown per page
+    postsPerPage: 3,
+
+    // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Daily Struggles`,
+
+    // This allows an site description for meta data for pages.
+    siteDescriptionMeta: `Struggles of a software developer`,
+
+    // Used for App and Offline manifest e.g. Mobile Home Screen
+    shortTitle: `Struggles of a software developer`,
+    siteIcon: `favicon.png`,
+    backgroundColor: `#e9e9e9`,
+    themeColor: `#15171A`,
+
+    // Show more logs for debugging purposes (default: false)
+    verbose: false,
+
+    // Severity for verbose mode: (`info`, `warn`, `error`)
+    severity: `info`,
+};
